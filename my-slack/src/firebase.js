f9b8c4bfe6c5b1a70bf096503fc31946b62ffe11
edd <zeedymarina@gmail.com>
2021-06-24 11:20:17 -0400

    first

---
+++ my-slack/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAhITZaj8vxu6f3fdaIt5akdOs1uFMsoDk",
+  authDomain: "my-slack-5ef7b.firebaseapp.com",
+  projectId: "my-slack-5ef7b",
+  storageBucket: "my-slack-5ef7b.appspot.com",
+  messagingSenderId: "17248468266",
+  appId: "1:17248468266:web:4d3c86629abaeddb87b253",
+};
