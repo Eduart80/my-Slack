4c7a10962911c0dd14c9e3a06e1e625822aa4bf0
edd <zeedymarina@gmail.com>
2021-06-24 12:32:26 -0400

    l

--- my-slack/src/App.js
+++ my-slack/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>react</h1>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Header />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
